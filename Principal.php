5607a22f4e0830231e5a3a1a76c556cf83488a07
ghpancardo <ghpancardo@gmail.com>
2024-05-17 23:52:50 -0600

    Corregir principal y modificar conexión

--- Principal.php
+++ Principal.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina Principal</title>
+    <title>Yogaweb</title>
 </head>
 <body >
 
@@ -12,14 +12,14 @@
 session_start();
 include "./logica/conexion.php";
 
-$nombre_usuario= $_SESSION['usermane'];
+$usuario= $_SESSION['usermane'];
 
-if(!isset($nombre_usuario)){
+if(!isset($usuario)){
 
     header("location: inicioSesion.html");
 
 }else{
-    $consulta_sql= "SELECT * FROM usuarios WHERE nombre_usuario='" .$nombre_usuario. "'";
+    $consulta_sql= "SELECT * FROM usuarios WHERE usuario='" .$usuario. "'";
     $resultado=$conexion->query($consulta_sql);
     $count=mysqli_num_rows($resultado);
     $row=mysqli_fetch_assoc($resultado);
@@ -27,7 +27,7 @@ if(!isset($nombre_usuario)){
 
     <div class="container w-75 bg-dark mt-3 rounded shadow">
         <div class="row">
-            <h1 style="color: white;" class="fw-bold text-center pt-4 mb-3"> Bienvenid@, <?php echo $row['nombre_usuario']; ?></h1> 
+            <h1 style="color: white;" class="fw-bold text-center pt-4 mb-3"> Bienvenid@, <?php echo $row['usuario']; ?></h1> 
             <h5 style="color: white;" class="fw-bold text-center pt-2 mb-3">Salir del curso <a href='logica/salir.php'> Cerrar Sesión</a></h5>
         </div>
     </div>
